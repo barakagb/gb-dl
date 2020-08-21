4fa423588211c2d7725965de99f131dfd74d7b26
barakagb <30619781+barakagb@users.noreply.github.com>
2020-08-21 07:41:25 +0300

    Update gb-dl.py

--- gb-dl.py
+++ gb-dl.py
@@ -1,2 +1,23 @@
-import gb-dl
-gb-dl()
+__version__ = "1.6.5"
+banner = '''  
+
+                   _____ ____                 _ _ 
+                  / ____|  _ \               | | |
+                 | |  __| |_) |  ______    __| | |
+                 | | |_ |  _ <  |______|  / _` | |
+                 | |__| | |_) |          | (_| | |
+                  \_____|____/            \__,_|_|
+
+           			        Version : ''' + __version__ + ''' 
+                            Author  : BarakaGB
+                            Visit   : https://github.com/barakagb/gb-dl
+                            Donation: https://www.buymeacoffee.com/barakagb
+                            Key url : https://bmc.xyz/l/gbdl
+                    '''
+print(banner)
+print(
+            '''    A python based utility to download courses from infosec4tc.teachable.com ,
+    ehacking.net ,stackskills.com and designerup.co ...etc for personal offline 
+    use.
+     \n''')
+print('''  Please run install and run gb-dl.py from pypi not this file   \n    ''')
